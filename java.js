884b6011c59d3ca2b71d4773896fdaf137dc9c83
Ryznyx <pepe170@gmail.com>
2025-04-16 21:45:56 +0200

    atm

--- java.js
+++ java.js
@@ -1,32 +1,14 @@
-const jobbtext  = document.getElementById('jobb');
-const baltext = document.getElementById('bal');
-const midgomb = document.getElementById('mid');
-let szamlalo = 0;
-
-
-
-midgomb.addEventListener('click', function () {
-    szamlalo++;
-
-    switch (szamlalo) {
-        case 1:
-            jobbtext.style.display = "none";
-
-            break;
-            
-        case 2:
-            baltext.style.display = "none";
-            break;
-        case 3:
-            jobbtext.style.display = "block";
-
-            break;
-        case 4: 
-            baltext.style.display = "block";
-            szamlalo = 0;
-            break;
-    }
-});
+//accstatus 1 enabled 0 disabled
+var validUsers = {
+    "Gyurcsány Ferenc": 
+    {
+    fullname: "Gyurcsány Ferenc",
+    userID: "gyferidk",
+    PIN: "0236",
+    balance: 100000000,
+    accountstatus: 1,
+    },
+};
 
 
 
@@ -45,20 +27,19 @@ midgomb.addEventListener('click', function () {
 
 
 
+/*Készítsünk egy webalkalmazást mely egy ATM automata működését próbálja utánozni.
 
+A layout leírása: 
+A képernyő bal oldalán kapjon helyet egy login felület: 
+- felhasználónév
+- egy input field ami csak számokat fogad el (ez a PIN kód)
+- login gomb
 
+A képernyő középső oldalán kapjanak helyet a bejelentkezett felhasználóval kapcsolatos információk:
+- teljes név
+- egyenleg
 
-
-
-
-/*3. Egyszerű weboldal
-
-A feladat egy egyszerű weboldal layoutjat kialakítani.
-Használj lorem ipsum generátort vagy a DK kampányszovegeit
-Bal oldalon egy hosszú szöveg, Jobb oldalon is egy hosszú szöveg, 
-a kettő között kicsi elvalasztas. 
-Az oldal szélétől is legyen kicsit elválasztva mindkét szöveg.
-Alattuk közepén egy gomb. Ha a gombot először nyomjuk meg a 
-jobb oldali szöveg tűnik el, ha másodjára a bal oldali tűnik el. 
-Ha harmadjára megnyomjuk a jobb oldali visszajön, ha negyedjere a bal oldali is. Ez ismétlődhet is.
+A képernyő jobb oldalán az withdraw felület lesz:
+- egy input field ami csak számokat fogad el
+- egy gomb ami a pénz "kivételére" szolgál
 */
