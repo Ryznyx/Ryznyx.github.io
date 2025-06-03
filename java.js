6d0fd951dd268f699d0b5587b832c1437344fb84
Ryznyx <pepe170@gmail.com>
2025-06-03 21:19:39 +0200

    test

--- java.js
+++ java.js
@@ -1,181 +1,63 @@
-let employeelist = [];
-employeelist.push({Név: "Kalányos Sztojka Iván", Cím: "lenaZucán", Személyiszám: "mija", Pozicio: "bokszoló", Fizetés: "3", Cigány: true});
-employeelist.push({Név: "Kolompár Orsós Sztojka Tamás", Cím: "Nyoccker", Személyiszám: "elvitték a desztók", Pozicio: "Ütőképes", Fizetés: "10", Cigány: true});
+ let todo = [];
+ todo.push( {name: "Boxolni verekedni boxolni", deadline: "2025/12/24", comment: "holnap bekell pukkasztani a bicepszet"});
+ todo.push( {name: "Kalanyos Terminator", deadline: "2025/12/23", comment: "holnap temető"});
+ todo.push( {name: "Don Gatto", deadline: "2025/12/23", comment: "elásni narit"});
+ todo.push( {name: "Félszemü babylon", deadline: "2025/12/26", comment: "csúszkálni"});
 
-let romamenuvalue = false;
-let hiddenfeature = document.getElementById("nameplateID"); 
-let count = 0;
-hiddenfeature.addEventListener("click", (event)=> {
-count++;
-if (count === 17){
-romamenuvalue = true;
-count = 0;
-alert("yoo cigany");
-}
-});
+ function updateDiv()
+{
 
+//tömb beolvasás
+let mainbox = document.getElementById("main");
+for (let index = 0; index < todo.length; index++) {
+    let element = todo[index];
 
-function mngmembers() {
+ //új card layout+data
+ let newDiv = document.createElement("div");
+ newDiv.classList.add("cardstyle");
+ let h4 = document.createElement("h5");
+ h4.textContent = "Név: "+ element.name;
 
+ let h42 = document.createElement("h4");
+ h42.textContent = "Határidő: " +element.deadline;
+
+ let h3 = document.createElement("h3");
+ h3.textContent = "Megjegyzés: "+element.comment;
+
+ //gombok
+ let donebutton = document.createElement("button");
+ donebutton.classList.add("buttondesign");
+ donebutton.innerText = ("Done");
+ let deletebutton = document.createElement("button");
+ deletebutton.innerText = ("Delete");
+ deletebutton.classList.add("buttondesign");
+
+//button functions 
+
+ deletebutton.onclick = function (){
+    mainbox.removeChild(newDiv);
+ }
+
+ donebutton.onclick = function(){
+    newDiv.classList.add("opacityhalf");
+    newDiv.removeChild(donebutton);
+ }
  
+ 
+    newDiv.appendChild(h4);
+    newDiv.appendChild(h42);
+    newDiv.appendChild(h3);
+    newDiv.appendChild(donebutton);
+    newDiv.appendChild(deletebutton); 
+    mainbox.appendChild(newDiv);
+   
 
-let dtlsvalue = document.getElementById("detailsdata");
-dtlsvalue.classList.remove("managemembersfield");
+  
 
-for (let index = 0; index < employeelist.length; index++) {
-    let element = employeelist[index];
-    
-    let newdiv = document.createElement("div");
 
-    let namedata = document.createElement("h5");
-    namedata.innerHTML = "Név: "+ element.Név;
-    newdiv.appendChild(namedata);
 
-    let namedata2 = document.createElement("h5");
-    namedata2.innerHTML = "Lakcím: "+ element.Cím;
-    newdiv.appendChild(namedata2);
-
-    let namedata3 = document.createElement("h5");
-    namedata3.innerHTML = "Személyi szám: "+ element.Személyiszám;
-    newdiv.appendChild(namedata3);
-
-    let namedata4 = document.createElement("h5");
-    namedata4.innerHTML = "Pozíció: "+element.Pozicio;
-    newdiv.appendChild(namedata4);
-
-    let namedata5 = document.createElement("h5");
-    namedata5.innerHTML = "Fizetés: "+ element.Fizetés;
-    newdiv.appendChild(namedata5);
-
-    let namedata6 = document.createElement("h5");
-    namedata6.innerHTML = "Subhuman status: " +element.Cigány;
-    newdiv.appendChild(namedata6);
-
-    let deletebutton = document.createElement("button");
-    deletebutton.innerText = "Delete";
-    newdiv.appendChild(deletebutton);
-
-    deletebutton.onclick = function() {
-        console.log(element);
-        employeelist.pop(element); // wip not working properly ||
-        dtlsvalue.removeChild(newdiv);
-        
-    }
-
-    newdiv.classList.add("employeecard");
-    if (element.cigany = true && romamenuvalue === true)
-    {
-        newdiv.classList.add("shitskin"); 
-    }
-    dtlsvalue.appendChild(newdiv);
-    
+ }
 }
-
-
-
-
-}
-
-function addmbrs(){
-    let  managevalue = document.getElementById("detailsdata");
-
-//inputlayout
-let fielddesc = document.createElement("h1");
-fielddesc.innerHTML = "Alkalmazott rögzítése";
-
-let name = document.createElement("div");
-name.innerHTML = "Név";
-let input = document.createElement("input");
-
-let address = document.createElement("div");
-address.innerHTML = "Lakcím";
-let input2 = document.createElement("input");
-
-let szemelyiszam = document.createElement("div");
-szemelyiszam.innerHTML = "Személyi szám";
-let input3 = document.createElement("input");
-
-let pozicio = document.createElement("div");
-pozicio.innerHTML = "Pozíció";
-let input4 = document.createElement("input");
-
-let fizetes = document.createElement("div");
-fizetes.innerHTML = "Fizetés";
-let input5 = document.createElement("input");
-
-let mainquestion = document.createElement("div");
-mainquestion.innerHTML ="ez egy redvás gedvás tetves rohadt cigány?";
-let romavalue = document.createElement("input");
-romavalue.type="checkbox";
-
-let addata = document.createElement("button");
-addata.innerText = "Hozzáadás a dolgozói listához";
-addata.onclick = function(){
-    addtoemployee();
-    emptyinputdata();
-    setTimeout(() => {
-    alert("Alkalmazott rögzítve");   
-    }, 600);
-    
-};
-
-function addtoemployee(){
-    
-employeelist.push({Név: input.value, Cím: input2.value, Személyiszám: input3.value, Pozicio: input4.value, Fizetés: input5.value, Cigány: romavalue.checked});
-console.log(employeelist);
-
-};
-function emptyinputdata(){
-    input.value='';
-    input2.value='';
-    input3.value='';
-    input4.value='';
-    input5.value='';
-    romavalue.checked = false;
-
-}
-
-//addtoDiv
-managevalue.appendChild(fielddesc);
-
-managevalue.appendChild(name);
-managevalue.appendChild(input);
-
-managevalue.appendChild(address);
-managevalue.appendChild(input2);
-
-managevalue.appendChild(szemelyiszam);
-managevalue.appendChild(input3);
-
-managevalue.appendChild(pozicio);
-managevalue.appendChild(input4);
-
-managevalue.appendChild(fizetes);
-managevalue.appendChild(input5);
-
-managevalue.appendChild(mainquestion);
-managevalue.appendChild(romavalue);
-
-managevalue.appendChild(addata);
-//addDesign
-mainquestion.classList.add("hideinfo");
-romavalue.classList.add("hideinfo");
-managevalue.classList.add("managemembersfield");
-addata.classList.add("buttondesign");
-fielddesc.classList.add("h1title");
-let nameplatefunction = document.getElementById("nameplateID"); //kikene szervezni innen
-let count = 0;
-nameplatefunction.addEventListener("click", (event)=> {
-count++;
-if (count === 17){
-mainquestion.classList.add("showinfo");
-romavalue.classList.add("showinfo");
-alert("yoo cgiany");
-romamenuvalue = true;
-count = 0;
-}
-});
-
-}
-
-
+setTimeout(() => {
+    updateDiv();
+}, 2000);
