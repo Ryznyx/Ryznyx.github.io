737a12e2f9141b238bdf05c9e4e35cb79664af4f
Ryznyx <pepe170@gmail.com>
2025-11-25 01:41:04 +0100

    c

--- java.js
+++ java.js
@@ -1,68 +1,56 @@
-let atvettword = "";
-let szolista = [];
-szolista.push("alma","kecske","komornyik","sárkány", "akar");
-console.log(szolista);
-let elteres = 0;
-let helyesszo = [];
-let asd = 0;
-let volthiba = false;
+let firstbutton = document.getElementById('kerdes1');
+let secondbutton = document.getElementById('kerdes2');
 
 
-function buttonclick(){
-atvettword = document.getElementById('textbox').value;
-var eredmeny = correction(szolista, atvettword);
-console.log(eredmeny);
-resetTodefault();
+
+let kerdesek = document.getElementById('B');
+
+
+
+function firstquestion()
+{
+kerdesek.innerHTML = `
+    <div>
+        <div >Ki a Sárkány? </div>
+        <button onclick=truevalasz();>Péter</button>
+        <button onclick=falsevalasz();>Offici</button>
+        <button onclick=falsevalasz();>Senki</button>
+    </div>
+`;
 }
 
-function correction(szolista, atvettword){
-    
-    for (let index = 0; index < szolista.length; index++) {
-        let teszt = szolista[index];  
-        elteres =0;
-        if (teszt.length === atvettword.length)
-        {
-            for (let indexx = 0; indexx < atvettword.length; indexx++) {
-                const element = atvettword[indexx];
-                const element2 = teszt[indexx];
-                if(element != element2)
-                {
-                    elteres++;
-                    volthiba = true;
-                }      
-            }
-        helyesszo.push({hiba:elteres,szó: teszt});
-        
-        }       
-    };
-
-    if (volthiba === true)
-    {
-        console.log(helyesszo);
-        for (let kek = 0; kek < helyesszo.length; kek++) {
-        let element = helyesszo[kek].hiba; 
-        console.log(element+ " currently az element");
-        if (element <= asd)
-        {
-            asd = kek;
-            console.log("asd = kek");
-        }
-        
-    };
-       return helyesszo[asd].szó; 
-    }
-    else{
-        return null;
-    }
-};
-
-function resetTodefault()
+function falsevalasz()
 {
-    atvettword = "";
-    helyesszo = [];
-    asd = 1;
-    volthiba = false;
-};
+    
+    alert("nemsikerült, try again");
+}
 
+function truevalasz()
+{
+  
+    alert("okoskutya");
+    secondbutton.disabled=false;
+    firstbutton.disabled=true;
+    kerdesek.innerHTML="";
+}
 
+function secondquestion(){
+kerdesek.innerHTML = `
+    <div>
+        <div >Hány fasz járt ma az offici anyjában? </div>
+        <button onclick=falsevalasz();>0</button>
+        <button onclick=falsevalasz();>2</button>
+        <button onclick=truevalasz2();>az int nem tárol ilyen nagy értéket</button>
+    </div>
+`;
 
+}
+
+function truevalasz2()
+{
+  
+    alert("okoskutya");
+    secondbutton.disabled=true;
+    firstbutton.disabled=true;
+    kerdesek.innerHTML="Végigvitted a játékot, gratulál az igaz sárkány";
+}
